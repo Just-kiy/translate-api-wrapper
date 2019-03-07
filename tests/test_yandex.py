a6a79e42a72ff9af1649dc4f97aeea63590b3041
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-07 15:17:23 +0300

    feat: added environs to tests

--- tests/test_yandex.py
+++ tests/test_yandex.py
@@ -1,10 +1,12 @@
 import pytest
+from environs import Env
 
 import json
 from translate_wrapper.yandex import YandexEngine
 
-API_KEY="trnsl.1.1.20190221T131631Z.1acbec869a7303cf.9b1b624de49bf0b1bd8bf754f0ba6a418881a90f"
-engine = YandexEngine(api_key=API_KEY)
+env = Env()
+env.read_env()
+engine = YandexEngine(api_key=env.str("YANDEX_API_KEY"))
 
 async def test_get_langs():
     r = await engine.get_langs("ru")
