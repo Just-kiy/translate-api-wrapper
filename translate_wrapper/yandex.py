fff260cb6f3b851f40943f80fcde93c210ad506f
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-07 12:09:30 +0300

    feat: Yandex Engine and tests are now async

---
+++ translate_wrapper/yandex.py
@@ -0,0 +1,40 @@
+from .engine import BaseEngine, BaseResponseConverter
+from pprint import pprint
+import aiohttp
+
+class YandexEngine(BaseEngine):
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.endpoint_api = "https://translate.yandex.net/api/v1.5/tr.json"
+
+    async def _send_request(self, url, body=None):
+        async with aiohttp.ClientSession() as session:
+            response = await session.post(url, data=body)
+            body = await response.json()
+            return YandexResponse(response, body)
+
+
+    def translate(self, text, lang, format="plain"):
+        url = f"{self.endpoint_api}/translate?key={self.api_key}&lang={lang}&format={format}"
+        body = {"text": text}
+        return self._send_request(url, body)
+
+    def get_langs(self, lang):
+        url = f"{self.endpoint_api}/getLangs?key={self.api_key}&ui={lang}"
+        return self._send_request(url)
+
+
+class YandexResponse(BaseResponseConverter):
+    def __init__(self, response, body):
+        super().__init__(response)
+        self.body = body
+
+
+class YandexServiceBuilder():
+    def __init__(self):
+        self._instance = None
+
+    def __call__(self, api_key):
+        if not self._instance:
+            self._instance = YandexEngine(api_key)
+        return self._instance
