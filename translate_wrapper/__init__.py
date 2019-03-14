3d8019a7d3e3909bc601a268c99900f9bd69d865
Just kiy <just_kiy@icloud.com>
2019-03-14 17:23:28 +0300

    Bumped version: 0.2.0 -> 0.3.0

--- translate_wrapper/__init__.py
+++ translate_wrapper/__init__.py
@@ -1,4 +1,14 @@
-from .yandex import YandexEngine
+from .bing import BingEngine
 from .google import GoogleEngine
+from .translators import factory
+from .yandex import YandexEngine
 
-__version__ = "0.2.0"
+__version__ = '0.3.0'
+
+__all__ = (
+    GoogleEngine,
+    YandexEngine,
+    BingEngine,
+    factory,
+    __version__,
+)
