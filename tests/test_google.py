ba301f169f8cbf4107875659cdf2818c8801f9db
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-20 16:02:57 +0300

    fix(test): fixed Google and Yandex E2E tests

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,26 +1,29 @@
 import pytest
 
+import os
+
 from environs import Env
 
 from translate_wrapper.google import GoogleEngine
 
-# TODO: fix it
-# env = Env()
-# env.read_env()
-# ENDPOINT_API = "https://translation.googleapis.com/language/translate/v2"
-# endpoint = env.str("GOOGLE_ENDPOINT", ENDPOINT_API)
-# engine = GoogleEngine(api_key=env.str("GOOGLE_API_KEY"), api_endpoint=endpoint)
+@pytest.fixture
+def google_engine():
+    env = Env()
+    env.read_env()
+    ENDPOINT_API = "https://translation.googleapis.com/language/translate/v2"
+    endpoint = env.str("GOOGLE_ENDPOINT", ENDPOINT_API)
+    return GoogleEngine(api_key=env.str("GOOGLE_API_KEY"), api_endpoint=endpoint)
 
 
-@pytest.mark.skip
-async def test_get_langs():
-    r = await engine.get_langs("ru")
+@pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
+async def test_get_langs(google_engine):
+    r = await google_engine.get_langs("ru")
     assert r.status == 200
     assert "languages" in r.body
 
 
-@pytest.mark.skip
-async def test_translate():
-    response = await engine.translate(text="Hello, World!", target="ru")
+@pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
+async def test_translate(google_engine):
+    response = await google_engine.translate(text="Hello, World!", target="ru")
     assert response.status == 200
     assert "translations" in response.body
--- tests/test_yandex.py
+++ tests/test_yandex.py
@@ -1,25 +1,28 @@
 import pytest
 
+import os
+
 from environs import Env
 
 from translate_wrapper.yandex import YandexEngine
 
-# TODO: fix it
-# env = Env()
-# env.read_env()
-# ENDPOINT_API = "https://translate.yandex.net/api/v1.5/tr.json"
-# endpoint = env.str("YANDEX_ENDPOIT", ENDPOINT_API)
-# engine = YandexEngine(api_key=env.str("YANDEX_API_KEY"), api_endpoint=endpoint)
+@pytest.fixture
+def yandex_engine():
+    env = Env()
+    env.read_env()
+    ENDPOINT_API = "https://translate.yandex.net/api/v1.5/tr.json"
+    endpoint = env.str("YANDEX_ENDPOIT", ENDPOINT_API)
+    return YandexEngine(api_key=env.str("YANDEX_API_KEY"), api_endpoint=endpoint)
 
 
-@pytest.mark.skip
-async def test_get_langs():
-    r = await engine.get_langs("ru")
+@pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
+async def test_get_langs(yandex_engine):
+    r = await yandex_engine.get_langs("ru")
     assert "dirs" in r.body
     assert "langs" in r.body
 
 
-@pytest.mark.skip
-async def test_translate():
-    response = await engine.translate("Hello, World!", "en-ru")
+@pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
+async def test_translate(yandex_engine):
+    response = await yandex_engine.translate("Hello, World!", "en-ru")
     assert response.status == 200
