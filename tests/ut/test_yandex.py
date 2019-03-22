2146814ac3f6af151d486c21039338a29a1abbb9
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-22 22:42:56 +0300

    feat(tests): added test_translate in unit tests for Yandex

--- tests/ut/test_yandex.py
+++ tests/ut/test_yandex.py
@@ -26,14 +26,31 @@ class YandexEngineTest:
     ])
     async def test_get_langs(self, mocked_send_request, yandex_engine, ui):
         yandex_engine._send_request = mocked_send_request
-
         assert await yandex_engine.get_langs(ui)
-
         expected = {
-            'url': 'http://yandex-server.test/getLangs',
+            'url': f'{yandex_engine.endpoint}/getLangs',
             'params': {
                 'ui': ui,
             }
         }
-
         mocked_send_request.assert_called_once_with(expected['url'], expected['params'])
+
+    @pytest.mark.parametrize('text, lang, format', [
+        ('Hello', 'ru', 'plain'),
+        ('Darkness', 'fr', 'plain'),
+        ('My old good friend', 'de', 'html'),
+    ])
+    async def test_translate(self, mocked_send_request, yandex_engine, text, lang, format):
+        yandex_engine._send_request = mocked_send_request
+        assert await yandex_engine.translate(text, lang, format)
+        expected = {
+            'url': f'{yandex_engine.endpoint}/translate',
+            'params': {
+                'lang': lang,
+                'format': format,
+            },
+            'body': {
+                'text': text
+            },
+        }
+        mocked_send_request.assert_called_once_with(expected['url'], expected['params'], expected['body'])
