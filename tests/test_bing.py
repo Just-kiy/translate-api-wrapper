736f04635817de54763989ef5b988cba42cb923e
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-22 20:42:16 +0300

    fix(tests): fixed e2e tests

--- tests/test_bing.py
+++ tests/test_bing.py
@@ -6,30 +6,35 @@ from environs import Env
 
 from translate_wrapper.bing import BingEngine
 
+pytestmark = [
+    pytest.mark.asyncio,
+    pytest.mark.e2e,
+    pytest.mark.bing,
+    pytest.mark.engines,
+    ]
 
 
 @pytest.fixture
-def bing_engine():
+def bing_engine(event_loop):
     env = Env()
     env.read_env()
-    ENDPOINT_API = 'https://api.cognitive.microsofttranslator.com'
-    API_V = '3.0'
-    endpoint = env.str('BING_ENDPOINT', ENDPOINT_API)
-    api_v = env.str('BING_API_V', API_V)
-    return BingEngine(api_key=env.str('BING_API_KEY'), api_endpoint=endpoint, api_v=api_v)
+    endpoint_api = 'https://api.cognitive.microsofttranslator.com'
+    api_version = '3.0'
+    endpoint = env.str('BING_ENDPOINT', endpoint_api)
+    api_v = env.str('BING_API_V', api_version)
+    return BingEngine(api_key=env.str('BING_API_KEY'), api_endpoint=endpoint, api_v=api_v, event_loop=event_loop)
 
 
 @pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
 async def test_get_langs(bing_engine):
-    r = await bing_engine.get_langs()
-
-    assert r.status == 200
-    assert 'translation' in r.body
+    response = await bing_engine.get_langs()
+    assert 'translation' in response
+    assert 'transliteration' in response
+    assert 'dictionary' in response
 
 
 @pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
 async def test_translate(bing_engine):
     response = await bing_engine.translate(text='Hello, World!', target='ru')
-
-    assert response.status == 200
-    assert 'translations' in response.body[0]
+    assert 'translations' in response[0]
+    assert 'ru' == response[0]['translations'][0]['to']
--- tests/test_yandex.py
+++ tests/test_yandex.py
@@ -6,23 +6,31 @@ from environs import Env
 
 from translate_wrapper.yandex import YandexEngine
 
+pytestmark = [
+    pytest.mark.asyncio,
+    pytest.mark.e2e,
+    pytest.mark.yandex,
+    pytest.mark.engines,
+    ]
+
+
 @pytest.fixture
-def yandex_engine():
+def yandex_engine(event_loop):
     env = Env()
     env.read_env()
-    ENDPOINT_API = 'https://translate.yandex.net/api/v1.5/tr.json'
-    endpoint = env.str('YANDEX_ENDPOIT', ENDPOINT_API)
-    return YandexEngine(api_key=env.str('YANDEX_API_KEY'), api_endpoint=endpoint)
+    endpoint_api = 'https://translate.yandex.net/api/v1.5/tr.json'
+    endpoint = env.str('YANDEX_ENDPOINT', endpoint_api)
+    return YandexEngine(api_key=env.str('YANDEX_API_KEY'), api_endpoint=endpoint, event_loop=event_loop)
 
 
 @pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
 async def test_get_langs(yandex_engine):
-    r = await yandex_engine.get_langs('ru')
-    assert 'dirs' in r.body
-    assert 'langs' in r.body
+    response = await yandex_engine.get_langs('ru')
+    assert 'dirs' in response
+    assert 'langs' in response
 
 
 @pytest.mark.skipif(not os.getenv('TESTING_E2E'), reason='expensive tests')
 async def test_translate(yandex_engine):
     response = await yandex_engine.translate('Hello, World!', 'en-ru')
-    assert response.status == 200
+    assert response['code'] == 200
