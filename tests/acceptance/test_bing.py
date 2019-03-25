32fda93ee82f22eea9dc91a415906214f93deb67
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-25 17:01:18 +0300

    Bing: acceptance tests

--- tests/acceptance/test_bing.py
+++ tests/acceptance/test_bing.py
@@ -4,7 +4,6 @@ from translate_wrapper.bing import BingEngine
 
 pytestmark = [
     pytest.mark.acceptance,
-
     pytest.mark.engines,
     pytest.mark.bing,
 ]
@@ -26,12 +25,9 @@ def bing_engine(event_loop, unused_tcp_port):
 class BingEngineTest:
     pytestmark = pytest.mark.asyncio
 
-    async def test_bla(self, unused_tcp_port, bing_engine: 'BingEngine'):
-        # NOTE: the testing echo server returns JSON Response
-        # with only one key "echo" and reflected value
+    async def test_get_langs(self, unused_tcp_port, bing_engine: 'BingEngine'):
         result = await bing_engine.get_langs()
 
-        # TODO: write a HTTP Request parser and use it instead
         assert result['echo'] == (
             'GET /languages?api-version=v1 HTTP/1.1\r\n'
             f'Host: 127.0.0.1:{unused_tcp_port}\r\n'
@@ -39,4 +35,31 @@ class BingEngineTest:
             'Ocp-Apim-Subscription-Key: api_key\r\n'
             'Accept: */*\r\nAccept-Encoding: gzip, deflate\r\n'
             'User-Agent: Python/3.7 aiohttp/3.5.4\r\n\r\n'
+        )
+
+    @pytest.mark.parametrize('text, target, source', [
+        ('One', 'ru', 'en'),
+        ('Two', 'de', None),
+        ('Three', 'fr', 'en'),
+        ('Four', 'it', None),
+    ])
+    async def test_translate(self, unused_tcp_port, bing_engine: 'BingEngine',
+                             text, target, source):
+        result = await bing_engine.translate(text, target, source)
+
+        content = [{'Text': text}]
+        to = f'to={target}'
+        _from = f'from={source}' if source else None
+        api_v = 'api-version=v1'
+        query = '&'.join((to, _from)) if _from else to
+
+        assert result['echo'] == (
+            f'POST /translate?{query}&{api_v} HTTP/1.1\r\n'
+            f'Host: 127.0.0.1:{unused_tcp_port}\r\n'
+            'Content-Type: application/json\r\n'
+            'Ocp-Apim-Subscription-Key: api_key\r\n'
+            'Accept: */*\r\nAccept-Encoding: gzip, deflate\r\n'
+            'User-Agent: Python/3.7 aiohttp/3.5.4\r\n'
+            f'Content-Length: {len(content.__str__())}\r\n\r\n'
+            f'{content}'
         )
