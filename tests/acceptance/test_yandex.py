00b885a23319d8aa8360ff363d976ee5860bc0d9
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-25 20:32:57 +0300

    Yandex: WIP acceptance tests

--- tests/acceptance/test_yandex.py
+++ tests/acceptance/test_yandex.py
@@ -2,20 +2,20 @@ import pytest
 import json
 import requests
 
-from translate_wrapper.bing import BingEngine
+from translate_wrapper.yandex import YandexEngine
 
 pytestmark = [
     pytest.mark.acceptance,
     pytest.mark.engines,
-    pytest.mark.bing,
+    pytest.mark.yandex,
 ]
 
 # SIMPLE TEST CASE EXAMPLE
 
 
 @pytest.fixture
-def bing_engine(event_loop, unused_tcp_port):
-    return BingEngine(
+def yandex_engine(event_loop, unused_tcp_port):
+    return YandexEngine(
         'api_key',
         f'http://127.0.0.1:{unused_tcp_port}',
         'v1',
@@ -24,13 +24,24 @@ def bing_engine(event_loop, unused_tcp_port):
 
 
 # noinspection PyMethodMayBeStatic
-class BingEngineTest:
+class YandexEngineTest:
     pytestmark = pytest.mark.asyncio
 
-    async def test_bla(self, unused_tcp_port, bing_engine: 'BingEngine'):
-        # NOTE: the testing echo server returns JSON Response
-        # with only one key "echo" and reflected value
-        result = await bing_engine.get_langs()
+    async def test_get_langs(self, unused_tcp_port, yandex_engine: 'YandexEngine'):
+        result = await yandex_engine.get_langs()
+
+        assert result['echo'] == (
+            'GET /languages?api-version=v1 HTTP/1.1\r\n'
+            f'Host: 127.0.0.1:{unused_tcp_port}\r\n'
+            'Content-Type: application/json\r\n'
+            'Ocp-Apim-Subscription-Key: api_key\r\n'
+            'Accept: */*\r\nAccept-Encoding: gzip, deflate\r\n'
+            'User-Agent: Python/3.7 aiohttp/3.5.4\r\n\r\n'
+        )
+
+
+    async def test_translate(self, unused_tcp_port, yandex_engine: 'YandexEngine'):
+        result = await yandex_engine.translate()
 
         assert result['echo'] == (
             'GET /languages?api-version=v1 HTTP/1.1\r\n'
