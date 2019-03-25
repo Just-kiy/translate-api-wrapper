4a07f76b3165b1a7c6ce06d28c2b4a9c2404f784
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-25 20:32:31 +0300

    Google: WIP acceptance tests

--- tests/acceptance/test_google.py
+++ tests/acceptance/test_google.py
@@ -1,42 +1,78 @@
 import pytest
-import json
-import requests
 
-from translate_wrapper.bing import BingEngine
+from .conftest import parse_response
+
+from translate_wrapper.google import GoogleEngine
 
 pytestmark = [
     pytest.mark.acceptance,
     pytest.mark.engines,
-    pytest.mark.bing,
+    pytest.mark.google,
 ]
 
 # SIMPLE TEST CASE EXAMPLE
 
 
 @pytest.fixture
-def bing_engine(event_loop, unused_tcp_port):
-    return BingEngine(
+def google_engine(event_loop, unused_tcp_port):
+    return GoogleEngine(
         'api_key',
         f'http://127.0.0.1:{unused_tcp_port}',
-        'v1',
         event_loop=event_loop
     )
 
 
 # noinspection PyMethodMayBeStatic
-class BingEngineTest:
+class GoogleEngineTest:
     pytestmark = pytest.mark.asyncio
 
-    async def test_bla(self, unused_tcp_port, bing_engine: 'BingEngine'):
+    @pytest.mark.parametrize('target, model', [
+        ('ru', 'nmt'),
+        ('ru', 'base'),
+        ('en', 'nmt'),
+        ('en', 'base'),
+    ])
+    async def test_get_langs(self, unused_tcp_port, google_engine: 'GoogleEngine',
+                             target, model):
         # NOTE: the testing echo server returns JSON Response
         # with only one key "echo" and reflected value
-        result = await bing_engine.get_langs()
+        response = await google_engine.get_langs(target, model)
 
-        assert result['echo'] == (
-            'GET /languages?api-version=v1 HTTP/1.1\r\n'
-            f'Host: 127.0.0.1:{unused_tcp_port}\r\n'
-            'Content-Type: application/json\r\n'
-            'Ocp-Apim-Subscription-Key: api_key\r\n'
-            'Accept: */*\r\nAccept-Encoding: gzip, deflate\r\n'
-            'User-Agent: Python/3.7 aiohttp/3.5.4\r\n\r\n'
-        )
+        query = '&'.join((
+            f'target={target}',
+            f'model={model}',
+            'key=api_key'
+        ))
+
+        result = parse_response(response['echo'])
+
+        assert result['Method'] == 'POST'
+        assert result['URL'] == f'/languages?{query}'
+        assert result['Host'] == f'127.0.0.1:{unused_tcp_port}'
+        assert result['User-Agent'] == 'Python/3.7 aiohttp/3.5.4'
+        assert result['Content-Type'] == 'application/octet-stream'
+
+    @pytest.mark.parametrize('text, target, source', [
+        ('Hello', 'ru', 'en'),
+        ('Hello', 'de', None),
+        ('Hello', 'fr', 'en'),
+        ('Hello', 'it', None),
+    ])
+    async def test_translate(self, unused_tcp_port, google_engine: 'GoogleEngine',
+                             text, target, source):
+        response = await google_engine.translate(text, target, source)
+
+        q = f'q={text}'
+        _target = f'target={target}'
+        _source = f'source={source}' if source else ''
+        key = f'key=api_key'
+
+        query = '&'.join((q, _target, _source, key))
+
+        result = parse_response(response['echo'])
+
+        assert result['Method'] == 'POST'
+        assert result['URL'] == f'/languages?{query}'
+        assert result['Host'] == f'127.0.0.1:{unused_tcp_port}'
+        assert result['User-Agent'] == 'Python/3.7 aiohttp/3.5.4'
+        assert result['Content-Type'] == 'application/json'
