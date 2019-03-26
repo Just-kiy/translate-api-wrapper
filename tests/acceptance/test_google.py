1add9aaae3cdece01c946f7d59ec9f3c6285fe58
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-26 09:30:01 +0300

    Google: fixed acceptance tests

--- tests/acceptance/test_google.py
+++ tests/acceptance/test_google.py
@@ -1,8 +1,10 @@
 import pytest
 
-from .conftest import parse_response
+import urllib.parse
 
 from translate_wrapper.google import GoogleEngine
+
+from .conftest import parse_response
 
 pytestmark = [
     pytest.mark.acceptance,
@@ -38,16 +40,17 @@ class GoogleEngineTest:
         # with only one key "echo" and reflected value
         response = await google_engine.get_langs(target, model)
 
-        query = '&'.join((
-            f'target={target}',
-            f'model={model}',
-            'key=api_key'
-        ))
+        params = {
+            'target': [target],
+            'model': [model],
+            'key': ['api_key']
+        }
 
         result = parse_response(response['echo'])
+        q_result = urllib.parse.urlparse(result['URL'])
 
         assert result['Method'] == 'POST'
-        assert result['URL'] == f'/languages?{query}'
+        assert urllib.parse.parse_qs(q_result[4]) == params
         assert result['Host'] == f'127.0.0.1:{unused_tcp_port}'
         assert result['User-Agent'] == 'Python/3.7 aiohttp/3.5.4'
         assert result['Content-Type'] == 'application/octet-stream'
@@ -62,17 +65,19 @@ class GoogleEngineTest:
                              text, target, source):
         response = await google_engine.translate(text, target, source)
 
-        q = f'q={text}'
-        _target = f'target={target}'
-        _source = f'source={source}' if source else ''
-        key = f'key=api_key'
-
-        query = '&'.join((q, _target, _source, key))
+        params = {
+            'target': [target],
+            'q': [text],
+            'key': ['api_key']
+        }
+        if source:
+            params['source'] = [source]
 
         result = parse_response(response['echo'])
+        q_result = urllib.parse.urlparse(result['URL'])
 
         assert result['Method'] == 'POST'
-        assert result['URL'] == f'/languages?{query}'
+        assert urllib.parse.parse_qs(q_result[4]) == params
         assert result['Host'] == f'127.0.0.1:{unused_tcp_port}'
         assert result['User-Agent'] == 'Python/3.7 aiohttp/3.5.4'
-        assert result['Content-Type'] == 'application/json'
+        assert result['Content-Type'] == 'application/octet-stream'
