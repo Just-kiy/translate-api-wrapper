670b2c47accb7997638b8bee7a2f30f98d97f0ff
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-26 11:04:23 +0300

    docs: updated __init__.py to contain version only

--- translate_wrapper/__init__.py
+++ translate_wrapper/__init__.py
@@ -1,14 +1,1 @@
-# from .bing import BingEngine
-# from .google import GoogleEngine
-# from .translators import factory
-# from .yandex import YandexEngine
-#
-# __version__ = '0.4.0'
-#
-# __all__ = (
-#     GoogleEngine,
-#     YandexEngine,
-#     BingEngine,
-#     factory,
-#     __version__,
-# )
+__version__ = '0.4.0'
