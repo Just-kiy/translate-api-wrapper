407b66c479ca358da966dc9b089a44b21059727b
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-27 16:49:38 +0300

    chore: removed ObjectFactory from engine.py

--- translate_wrapper/engine.py
+++ translate_wrapper/engine.py
@@ -18,17 +18,3 @@ class BaseResponseConverter:
         self.method = response.method
         self.url = response.url
         self.headers = response.headers
-
-
-class ObjectFactory:
-    def __init__(self):
-        self._builders = {}
-
-    def register(self, name, builder):
-        self._builders[name] = builder
-
-    def create(self, name, **kwargs):
-        builder = self._builders.get(name)
-        if not builder:
-            raise ValueError(name)
-        return builder(**kwargs)
