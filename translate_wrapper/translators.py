fc555a545457d80c14114a81cd189634d42435fb
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-27 18:05:25 +0300

    feat: Translator: added Translator (wrapper) class, refactored factory

--- translate_wrapper/translators.py
+++ translate_wrapper/translators.py
@@ -1,14 +1,29 @@
 from .engine import BaseEngine
 
+import typing as t
+
+
+class Translator:
+    def __init__(self, engine: BaseEngine, config: t.Dict):
+        self._engine = engine(**config)
+
+    def get_languages(self, **kwargs):
+        response = self._engine.get_langs(**kwargs)
+        return response
+
+    def translate(self, **kwargs):
+        response = self._engine.translate(**kwargs)
+        return response
+
 
 class TranslatorFactory:
-    _engines = {}
+    _translators = {}
 
-    def register_engine(self, name: str, implementation: BaseEngine):
-        self._engines[name] = implementation
+    def register_translator(self, name: str, implementation: BaseEngine, config: t.Dict):
+        self._translators[name] = Translator(implementation, config)
 
     def get_translator(self, name: str):
-        translator = self._engines.get(name)
+        translator = self._translators.get(name)
         if not translator:
             raise ValueError(name)
         return translator
