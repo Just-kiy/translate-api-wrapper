0e8a399e30356d74d75e36a757a112b5ae8049ba
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-27 18:19:48 +0300

    fix(tests): Translator: fix unit tests

--- tests/ut/test_translator.py
+++ tests/ut/test_translator.py
@@ -1,9 +1,6 @@
 import pytest
 
-import typing as t
-
 from translate_wrapper.bing import BingEngine
-from translate_wrapper.engine import BaseEngine
 from translate_wrapper.google import GoogleEngine
 from translate_wrapper.translators import TranslatorFactory
 from translate_wrapper.yandex import YandexEngine
@@ -17,15 +14,31 @@ pytestmark = [
 engines = [
     {
         'name': 'yandex',
-        'implementation': YandexEngine
-    },
-    {
-        'name': 'bing',
-        'implementation': BingEngine
+        'implementation': YandexEngine,
+        'config': {
+            'api_key': 'API_KEY',
+            'api_endpoint': 'API_ENDPOINT',
+            'event_loop': None,
+        }
     },
     {
         'name': 'google',
-        'implementation': GoogleEngine
+        'implementation': GoogleEngine,
+        'config': {
+            'api_key': 'API_KEY',
+            'api_endpoint': 'API_ENDPOINT',
+            'event_loop': None,
+        },
+    },
+    {
+        'name': 'bing',
+        'implementation': BingEngine,
+        'config': {
+            'api_key': 'API_KEY',
+            'api_endpoint': 'API_ENDPOINT',
+            'api_v': 'API_V',
+            'event_loop': None,
+        },
     },
 ]
 
@@ -37,12 +50,12 @@ def factory():
 
 class TranslatorTest:
     @pytest.mark.parametrize('engine', engines)
-    def test_register_engine(self, engine, factory):
-        factory.register_engine(**engine)
-        assert engine['name'] in factory._engines
+    def test_register_translator(self, engine, factory):
+        factory.register_translator(**engine)
+        assert engine['name'] in factory._translators
 
     @pytest.mark.parametrize('engine', engines)
     def test_get_translator(self, engine, factory):
-        factory.register_engine(**engine)
+        factory.register_translator(**engine)
         translator = factory.get_translator(engine['name'])
-        assert issubclass(translator, BaseEngine)
+        assert isinstance(translator._engine, engine['implementation'])
