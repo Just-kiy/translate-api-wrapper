0a2fd8408174b5cebc255e3b9a7c3f57a27ed08c
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-29 15:07:37 +0300

    fix: Tests - commented incomplete tests

--- tests/ut/test_main.py
+++ tests/ut/test_main.py
@@ -1,12 +1,12 @@
-from translate_wrapper.translators import Translator
-
-
-@pytest.mark.acceptance
-def test_translation():
-    t = Translator()
-    t.get_languages()
-    ...
-
-
-def test_yandex_get_langs_response_handle():
-    pass
+# from translate_wrapper.translators import Translator
+#
+#
+# @pytest.mark.acceptance
+# def test_translation():
+#     t = Translator()
+#     t.get_languages()
+#     ...
+#
+#
+# def test_yandex_get_langs_response_handle():
+#     pass
