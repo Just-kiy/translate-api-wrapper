0104a86a1aeb5a1bd41ece29acb6c5bfff2b4b53
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-29 18:49:54 +0300

    WIP: Translator

--- translate_wrapper/translators.py
+++ translate_wrapper/translators.py
@@ -1,6 +1,6 @@
-from .engine import BaseEngine
-
 import typing as t
+
+from .engine import BaseEngine
 
 
 class Translator:
@@ -14,64 +14,55 @@ class Translator:
     >>> translator = Translator.get_translator('yandex', api_key='some_key')
     >>> translator.get_languages()
     ['en', 'ru']
-    >>> translator.is_language_supported('en')
-    True
     >>> translator.translate(source='en', target='es', text='Hello, word!')
     'Hola palabra'
     """
     def __init__(self, engine: t.Type['BaseEngine'], config: dict):
         self._engine = engine(**config)
 
-    def get_languages(self) -> t.List[str]:
-        response = self._engine.get_langs()
-        # NOTE: if some error occurs ResponseHandler::resolve_get_langs(...) should resolve it too
-        return self._response_handler.resolve_get_langs(response)
+    def get_languages(self, target_language: t.Optional[str]) -> t.List[str]:
+        response = self._engine.get_languages(target_language)
+        return response
 
-    def translate(self, *, source: str, target: str, text: str) -> str:
+    def translate(self, *, source: str, target: str, text: str) -> t.List[str]:
         response = self._engine.translate(source=source, target=target, text=text)
-        return self._response_handler.resolve_translate(response)
+        return response
 
     @classmethod
     def get_translator(cls, translate_machine_name: str, *, api_key: str) -> 'Translator':
-        # it's how in python the factory pattern looks like
-
+        # TODO: Errors
         if translate_machine_name not in tm_receptionist:
             raise YouScrewedUpError
 
-        translate_machine, response_handler = tm_receptionist[translate_machine_name]  # TODO
-        return cls(translate_machine=translate_machine, response_handler=response_handler)
+        translate_machine = tm_receptionist[translate_machine_name]
+        return cls(translate_machine=translate_machine)
 
 
-class TranslatorFactory:  # TODO: rename it to `TranslateMachine`
+class TranslatorMachine:
+    # TODO: update docstring after all
     """
     Translate Machine Receptionist is an object that register translation machines and their handlers.
 
     >>> from translate_wrapper.tm_receptionist import tm_receptionist
-    >>> from translate_wrapper.yandex import YandexEngine as YandexTranslationMachine
-    >>> from translate_wrapper.response_handlers import YandexResponseHandler
-    >>> tm_receptionist.register(machine=YandexTranslationMachine, response_handler=YandexResponseHandler)
+    >>> from translate_wrapper.yandex import YandexEngine as YandexEngine
+    >>> tm_receptionist.register(machine=YandexEngine)
     >>> from translate_wrapper.google import GoogleEngine
     >>> tm_receptionist.register(machine=GoogleEngine)  # ok
     """
 
     def __init__(self):
-        self._translators = {}
+        self._translator_machines = {}
+
+    def __contains__(self, item):
+        return item in self._translator_machines
+
+    def __getitem__(self, key):
+        return self._translator_machines[key]
 
     # TODO: implement it
-    def register(self, *, machine: t.Type['BaseEngine'],
-                 response_handler: t.Optional[t.Type['BaseResponseHandler']] = None):
-        response_handler = default_response_handler if response_handler is None else response_handler
-        pass
-
-    def register_translator(self, name: str, implementation: BaseEngine, config: t.Dict):
-        self._translators[name] = Translator(implementation, config)
-
-    def get_translator(self, name: str):
-        translator = self._translators.get(name)
-        if not translator:
-            raise ValueError(name)
-        return translator
+    def register(self, *, machine: t.Type['BaseEngine']):
+        self._translator_machines[machine]
 
 
-tm_receptionist = TranslatorFactory()
-del TranslatorFactory  # TODO: you can skip it
+tm_receptionist = TranslatorMachine()
+del TranslatorMachine  # TODO: you can skip it
