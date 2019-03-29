bbaf189a9d66cbf1244e2f755e76a7327f73d0a1
Kovalev Nikita <kovalev.n@cronix.ms>
2019-03-29 17:47:25 +0300

    chore: added typing and docstrings to BaseEngine

--- translate_wrapper/engine.py
+++ translate_wrapper/engine.py
@@ -1,4 +1,4 @@
-import typing as t  # TODO
+import typing as t
 
 
 class BaseEngine:
@@ -6,31 +6,59 @@ class BaseEngine:
     Base class for all translate machines.
 
     All subclasses should be able to interact with an appropriate translation service.
-    In other words, all children have only one responsibility: interaction via HTTP.
 
     All specific settings instances of `TranslateMachine` should get from ENV.
     """
     def __init__(self, **kwargs):
         pass
 
-    def translate(self, **kwargs):
+    def translate(self, text: t.Optional[str, t.List[str]], target: str, source: t.Optional[str]) -> t.List[str]:
+        """
+        Translate given text (single string or list of strings) from source (optional) language to target one.
+        :param text:
+        :param target:
+        :param source:
+        :return: List of translated texts
+        """
         raise NotImplementedError
 
-    def get_langs(self, **kwargs):
+    def get_langs(self, language: t.Optional[str]) -> t.List[str]:
+        """
+        Takes language and return list of languages (BCP-47), which are available as translate direction from given one
+        :param language: Source language
+        :return: List of target languages
+        """
         raise NotImplementedError
 
-    def _send_request(self, **kwargs):
+    def _send_request(self, **kwargs) -> t.Dict:
+        """
+        Sending request to Translation Service. Both for get_langs and translate methods.
+        :param kwargs:
+        :return: JSON
+        """
         raise NotImplementedError
 
-    def convert_response(self, **kwargs):
-        # TODO: it messes all up, it's not good that an engine can know the strategy how to convert a response
+    def convert_response(self, method: str, response: t.Dict) -> t.List[str]:
+        """
+        Wrapper. Takes method and dispatch response to it
+        :param (Str) method: 'get_langs'|'translate"
+        :param (Dict) response: JSON from Translation Service
+        :return: (List) Converted response
+        """
         raise NotImplementedError
 
+    def _convert_langs(self, response: t.Dict) -> t.List[str]:
+        """
+        Takes response, selects target languages and return them as list
+        :param response: JSON from Translation Service
+        :return: List of target languages
+        """
+        raise NotImplementedError
 
-class BaseResponseConverter:
-    def __init__(self, response):
-        self.status = response.status
-        self.reason = response.reason
-        self.method = response.method
-        self.url = response.url
-        self.headers = response.headers
+    def _convert_translate(self, response: t.Dict) -> t.List[str]:
+        """
+        Takes response, select only translated texts and return them as list
+        :param response: JSON from Translation Service
+        :return: List of translated texts
+        """
+        raise NotImplementedError
