da3f904453b5a0664d7a0df3ea4b6a006a1fd499
Kovalev Nikita <kovalev.n@cronix.ms>
2019-04-01 18:26:44 +0300

    feat: Translator - implemented factory; fixed async methods

--- translate_wrapper/translators.py
+++ translate_wrapper/translators.py
@@ -1,5 +1,7 @@
 import typing as t
 
+import asyncio
+
 from .engine import BaseEngine
 
 
@@ -9,10 +11,9 @@ class Translator:
 
     It has knowledge of engines, their types, their responses and response handlers.
     It delegates responsibility to communicate with real outer translation services to engines.
-    It doesn't handle a translation service response on his own, it delegates it to an instance of `ResponseHandler`.
 
     >>> translator = Translator.get_translator('yandex', api_key='some_key')
-    >>> translator.get_languages()
+    >>> translator.get_languages('es') # NOTE: (Only BCP-47 codes!)
     ['en', 'ru']
     >>> translator.translate(source='en', target='es', text='Hello, word!')
     'Hola palabra'
@@ -22,32 +23,31 @@ class Translator:
 
     def get_languages(self, target_language: t.Optional[str]) -> t.List[str]:
         response = self._engine.get_languages(target_language)
-        return response
+        return asyncio.run(response)
 
-    def translate(self, *, source: str, target: str, text: str) -> t.List[str]:
+    def translate(self, *, source: t.Optional[str], target: str, text: t.Union[str, t.List[str]]) -> t.List[str]:
         response = self._engine.translate(source=source, target=target, text=text)
-        return response
+        return asyncio.run(response)
 
     @classmethod
-    def get_translator(cls, translate_machine_name: str, *, api_key: str) -> 'Translator':
-        # TODO: Errors
-        if translate_machine_name not in tm_receptionist:
-            raise YouScrewedUpError
+    def get_translator(cls, translator_name: str, api_key: str) -> 'Translator':
 
-        translate_machine = tm_receptionist[translate_machine_name]
-        return cls(translate_machine=translate_machine)
+        assert translator_name in translate_engines, \
+            f"{translator_name} is not registered Translate Engine! Use translate_engines.register first."
+
+        engine = translate_engines[translator_name]
+        return cls(engine=engine, config={'api_key': api_key})
 
 
-class TranslatorMachine:
-    # TODO: update docstring after all
+class _TranslateEngines:
     """
-    Translate Machine Receptionist is an object that register translation machines and their handlers.
+    Translate Engines is an object that register translation engines for factory purposes.
 
-    >>> from translate_wrapper.tm_receptionist import tm_receptionist
-    >>> from translate_wrapper.yandex import YandexEngine as YandexEngine
-    >>> tm_receptionist.register(machine=YandexEngine)
+    >>> from translate_wrapper.translators import translate_engines
+    >>> from translate_wrapper.yandex import YandexEngine
+    >>> translate_engines.register(translator_name='Yandex', engine=YandexEngine)
     >>> from translate_wrapper.google import GoogleEngine
-    >>> tm_receptionist.register(machine=GoogleEngine)  # ok
+    >>> translate_engines.register(translator_name='Google', engine=GoogleEngine)  # ok
     """
 
     def __init__(self):
@@ -59,10 +59,8 @@ class TranslatorMachine:
     def __getitem__(self, key):
         return self._translator_machines[key]
 
-    # TODO: implement it
-    def register(self, *, machine: t.Type['BaseEngine']):
-        self._translator_machines[machine]
+    def register(self, *, translator_name: str, engine: t.Type['BaseEngine']):
+        self._translator_machines[translator_name] = engine
 
 
-tm_receptionist = TranslatorMachine()
-del TranslatorMachine  # TODO: you can skip it
+translate_engines = _TranslateEngines()
