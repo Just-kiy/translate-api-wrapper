890f6198339e52f24188b44889ec18118d43f152
Kovalev Nikita <kovalev.n@cronix.ms>
2019-04-04 17:05:01 +0300

    fix: Translator - say NO to magic numbers

--- translate_wrapper/translators.py
+++ translate_wrapper/translators.py
@@ -18,15 +18,17 @@ class Translator:
     >>> translator.translate(source='en', target='es', text='Hello, word!')
     'Hola palabra'
     """
+
     def __init__(self, engine: BaseEngine):
         self._engine = engine
+        self.TRANSLATE_TEXT_CHUNK_SIZE = 10
 
     async def get_languages(self, target_language: t.Optional[str]) -> t.List[str]:
         response = await self._engine.get_languages(target_language)
         return response
 
     async def translate(self, *text: str, source: t.Optional[str], target: str) -> t.List[str]:
-        for chunk in funcy.chunks(10, *text):
+        for chunk in funcy.chunks(self.TRANSLATE_TEXT_CHUNK_SIZE, *text):
             print(chunk)
         response = await self._engine.translate(source=source, target=target, text=text)
         return response
