f2c52d7cd280e04d0efa7838d695feaa4319cab5
Kovalev Nikita <kovalev.n@cronix.ms>
2019-04-04 15:47:16 +0300

    chore: Engine - removed private methods from interface class BaseEngine

--- translate_wrapper/engines/engine.py
+++ translate_wrapper/engines/engine.py
@@ -8,6 +8,8 @@ class BaseEngine:
     All subclasses should be able to interact with an appropriate translation service.
 
     All specific settings instances of `TranslateMachine` should get from ENV.
+
+    Subclasses must implement convert response strategy and method to send request to proper Translation Service
     """
     def translate(self, text: t.Union[str, t.List[str]], target: str, source: t.Optional[str]) -> t.List[str]:
         """
@@ -27,35 +29,11 @@ class BaseEngine:
         """
         raise NotImplementedError
 
-    def _send_request(self, **kwargs) -> t.Dict:
-        """
-        Sending request to Translation Service. Both for get_langs and translate methods.
-        :param kwargs:
-        :return: JSON
-        """
-        raise NotImplementedError
-
     def convert_response(self, method: str, response: t.Dict) -> t.List[str]:
         """
-        Wrapper. Takes method and dispatch response to it
+        Wrapper. Takes method name and dispatch response to it
         :param (Str) method: 'get_langs'|'translate"
         :param (Dict) response: JSON from Translation Service
         :return: (List) Converted response
-        """
-        raise NotImplementedError
-
-    def _convert_langs(self, response: t.Dict) -> t.List[str]:
-        """
-        Takes response, selects target languages and return them as list
-        :param response: JSON from Translation Service
-        :return: List of target languages
-        """
-        raise NotImplementedError
-
-    def _convert_translate(self, response: t.Dict) -> t.List[str]:
-        """
-        Takes response, select only translated texts and return them as list
-        :param response: JSON from Translation Service
-        :return: List of translated texts
         """
         raise NotImplementedError
