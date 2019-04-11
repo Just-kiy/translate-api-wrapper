c30399c2b131a6914cc6ed482d26a986a3d53150
Kovalev Nikita <kovalev.n@cronix.ms>
2019-04-11 11:01:54 +0300

    tweak: Project - pre-registered all Engines to translate_engine factory

--- translate_wrapper/__init__.py
+++ translate_wrapper/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '0.4.0'
+
+from .engines.bing import BingEngine
+from .engines.google import GoogleEngine
+from .engines.yandex import YandexEngine
+from .translators import translate_engines, Translator
+
+translate_engines.register(engine=BingEngine)
+translate_engines.register(engine=GoogleEngine)
+translate_engines.register(engine=YandexEngine)
+
+__all__ = [
+    'translate_engines',
+    'Translator',
+    '__version__',
+]
