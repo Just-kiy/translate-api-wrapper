956776b57fa5e80462752ee5d5a033db5f8966d4
Kovalev Nikita <kovalev.n@cronix.ms>
2019-04-15 11:25:39 +0300

    fix: Tests - fixed UT for Translator

--- tests/ut/test_translator.py
+++ tests/ut/test_translator.py
@@ -3,23 +3,23 @@ import pytest
 from translate_wrapper.engines.bing import BingEngine
 from translate_wrapper.engines.google import GoogleEngine
 from translate_wrapper.engines.yandex import YandexEngine
-from translate_wrapper.translators import Translator, translate_engines
+from translate_wrapper.translators import Translator, _TranslateEngines
 
 
 @pytest.fixture
 def engines():
-    _engines = translate_engines
-    _engines.register(translator_name='yandex', engine=YandexEngine)
-    _engines.register(translator_name='google', engine=GoogleEngine)
-    _engines.register(translator_name='bing', engine=BingEngine)
+    _engines = _TranslateEngines()
+    _engines.register(engine=YandexEngine)
+    _engines.register(engine=GoogleEngine)
+    _engines.register(engine=BingEngine)
     return _engines
 
 
 class TranslatorTest:
     @pytest.mark.parametrize('translator_name, engine', [
-        ('yandex', YandexEngine),
-        ('google', GoogleEngine),
-        ('bing', BingEngine),
+        ('Yandex', YandexEngine),
+        ('Google', GoogleEngine),
+        ('Bing', BingEngine),
     ])
     def test_get_translator(self, engines, translator_name, engine):
         translator = Translator.get_translator(translator_name=translator_name, api_key='some_api_key')
